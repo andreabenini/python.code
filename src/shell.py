7f7073b38bc290e514e2bb44c87b0b544cb1894e
Ben <andreabenini@users.noreply.github.com>
2017-09-20 09:30:05 +0200

    Create shell.py

---
+++ src/shell.py
@@ -0,0 +1,18 @@
+# EXEC COMMAND - Execute a command on the current shell
+# @param Command (array|string) Something to execute, it might be an array or a string
+def ExecCommand(Command=[], DevNull=True):
+    # default:Devnull, otherwise redirected on stdout
+    StdErr = open('/dev/null', 'w') if DevNull else subprocess.STDOUT
+    if isinstance(Command, (list, tuple)):          # An array, executed "normally"
+        Process = subprocess.Popen(Command, stdout=subprocess.PIPE, stderr=StdErr)
+    else:                                           # Command is a string, running it into a shell
+        Process = subprocess.Popen(Command, stdout=subprocess.PIPE, stderr=StdErr, shell=True)
+    Process.wait()
+    return Process.communicate()[0], Process.returncode
+#
+# Output, ReturnCode = Config.ExecCommand('systemctl daemon-reload')
+# if ReturnCode is not 0:
+#     print('something went wrong (%s)' % ReturnCode)
+# else:
+#     print('Output: %s' % Output)
+        
