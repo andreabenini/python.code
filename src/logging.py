008b979ef79ce42635d7bc69ba9bc3cb1b2efcd9
Ben <andreabenini@users.noreply.github.com>
2017-10-25 12:23:58 +0200

    Create logging.py

---
+++ src/logging.py
@@ -0,0 +1,36 @@
+# Custom logging with attributes inherited, custom prints for each single class
+# @see:  Python website is your best friend even if logging doc is a mess
+#        - https://docs.python.org/3/howto/logging.html
+#        - https://docs.python.org/3/howto/logging-cookbook.html
+import logging
+
+def LoggerSetup():
+    logger = logging.getLogger("Something")
+    logger.setLevel(logging.DEBUG)
+    Formatter   = logging.Formatter('%(asctime)-15s [%(levelname)-8s] %(message)s', '%Y-%m-%d %H:%M:%S')
+    FileHandler = logging.FileHandler('/tmp/log.log')
+    FileHandler.setFormatter(Formatter)
+    logger.addHandler(FileHandler)
+    logger.info("")
+    logger.info("Log Started")
+
+class CustomAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        return '%-12s %s' % (self.extra['class'], msg), kwargs
+
+class BaseClass():
+    def __init__(self):
+        self.name   = self.__class__.__name__
+        self.logger = CustomAdapter(logging.getLogger('Something'), {'class': self.name})
+        # was: self.logger = logging.getLogger("Something")
+        self.logger.info("inherited log")
+
+
+class DerivedClass(BaseClass):
+    def __init__(self):
+        super(DerivedClass, self).__init__()
+        self.logger.info("[Derived] rules now !")
+
+LoggerSetup()
+B = BaseClass()
+D = DerivedClass()
