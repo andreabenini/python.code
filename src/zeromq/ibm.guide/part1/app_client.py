e34cc491159441c61f8e9c73386d0ce760c79cf3
Ben <andreabenini@users.noreply.github.com>
2018-03-24 10:53:55 +0100

    Update app_client.py

--- src/zeromq/ibm.guide/part1/app_client.py
+++ src/zeromq/ibm.guide/part1/app_client.py
@@ -1,60 +1,71 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys
 import zmq
-
 from datetime import datetime, timedelta
-from zmq.eventloop.ioloop import IOLoop, PeriodicCallback
+from zmq.eventloop.ioloop    import IOLoop
+from zmq.eventloop.ioloop    import PeriodicCallback
 from zmq.eventloop.zmqstream import ZMQStream
 from zmq_msg_helo import *
 
 
 class AppClient(object):
+    def __init__(self):
+        self.ctx = zmq.Context()
+        self.loop = IOLoop.instance()
+        self.endpoint = "tcp://127.0.0.1:5556"
+        self.client = self.ctx.socket(zmq.DEALER)
+        self.client.setsockopt(zmq.LINGER, 0)       # Without linger and timeouts you might have problems when closing context
+        self.client.setsockopt(zmq.RCVTIMEO, 5000)  # 5s
+        self.client.setsockopt(zmq.SNDTIMEO, 5000)
+        print("Connecting to", self.endpoint)
+        self.client.connect(self.endpoint)
+        self.client = ZMQStream(self.client)
+        self.client.on_recv(self.on_recv)
+        self.periodic = PeriodicCallback(self.periodictask, 1000)
+        self.last_recv = None
 
-	endpoint = "tcp://127.0.0.1:5556"
 
-	def __init__(self):
+    def disconnect(self):
+        if self.ctx is not None:
+            try:
+                self.periodic.stop()
+                print("Closing socket and context")
+                self.client.close()
+                self.ctx.term()
+            except Exception as e:
+                print(e)
 
-		self.ctx = zmq.Context()
-		self.loop = IOLoop.instance()
 
-		self.client = self.ctx.socket(zmq.DEALER)
+    def periodictask(self):
+        if self.client is None:
+            return
+        if not self.last_recv or self.last_recv + timedelta(seconds=5) < datetime.utcnow():
+            print("No data from remote (5s)... [ping]")
+        print("Sending HELLO to server")
+        msg = HelloMessage()
+        msg.send(self.client)
 
-		self.client.connect(self.endpoint)
-		print("Connecting to", self.endpoint)
-		self.client = ZMQStream(self.client)
 
-		self.client.on_recv(self.on_recv)
+    def start(self):
+        try:
+            self.periodic.start()
+            self.loop.start()
+            msg = HelloMessage()
+            msg.send(self.client)
+        except KeyboardInterrupt:
+            print("\n\nCtrl+C detected\n")
+        except Exception as E:
+            print("WTF ! Error detected")
+            print(E)
+        finally:
+            self.disconnect()
 
-		self.periodic = PeriodicCallback(self.periodictask, 1000)
 
-		self.last_recv = None
+    def on_recv(self, msg):
+        self.last_recv = datetime.utcnow()
+        print("Received a message of type %s from server!" % msg[0])
 
-	def periodictask(self):
-
-		if not self.last_recv or self.last_recv + timedelta(seconds=5) < datetime.utcnow():
-			print("Hmmm... haven't heard from the server in 5 seconds... Server unresponsive.")
-
-		print("Sending HELLO to server")
-		msg = HelloMessage()
-		msg.send(self.client)
-
-	def start(self):
-
-		self.periodic.start()
-		try:
-			self.loop.start()
-		except KeyboardInterrupt:
-			pass
-
-	def on_recv(self, msg):
-
-		self.last_recv = datetime.utcnow()
-
-		print("Received a message of type %s from server!" % msg[0])
 
 if __name__ == '__main__':
-	my_client = AppClient()
-	my_client.start()
-
-#
+    my_client = AppClient()
+    my_client.start()
