5cac70a375e3ac80628f085730e10f6d6df3bcc5
Ben <andreabenini@users.noreply.github.com>
2018-06-08 09:26:36 +0200

    uWSGI Hello World
    Nothing fancy here, just an hello world application

--- src/uwsgi/sample.py
+++ src/uwsgi/sample.py
@@ -1,9 +1,9 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    yield b"Hello World<br>"
     yield b"Row One<br>"
     yield b"Row Two<br>"
     yield b"Row Three<br><xmp>"
     for Item in env:
         yield ("%s = %s\n" % (Item, env[Item])).encode('UTF-8')
     yield b"</xmp>"
-    # return [b"Hello World :)"]
