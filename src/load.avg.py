b7f08231c28d1bcd4880b189b3e50715ecbc8928
Ben <andreabenini@users.noreply.github.com>
2018-12-19 10:51:49 +0100

    Create load.avg.py

---
+++ src/load.avg.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+#
+# LoadAVG every 1s:   1m 5m 15m
+#         Just like top but without too much info from there
+#
+import sys
+import time
+
+try:
+    while True:
+        with open('/proc/loadavg', 'r') as myfile:
+            data = myfile.read().split(' ', 4)
+            sys.stdout.write("\r{0} {1} {2}".format(data[0], data[1], data[2]))
+        sys.stdout.flush()
+        time.sleep(1)
+except:
+    print()
+    pass
