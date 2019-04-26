7ef1b67102ec91f7367c23e64ff9ccd84a6269f9
Ben <andreabenini@users.noreply.github.com>
2019-04-26 10:52:05 +0200

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -6,3 +6,15 @@ def __SignalHandler(signal, frame):  # @UnusedVariable
 # Ctrl+C signal handler in a program
 import signal
 signal.signal(signal.SIGINT, __SignalHandler)
+
+
+
+import sys
+
+# CHECK PYTHON - Check if current python version is suitable for the program
+# @return (boolean) True|False if python version is fine or not
+def checkPython(Version=3):
+    if sys.version_info[0] >= Version:
+        return True
+    print("Python v{} required for this program".format(Version))
+    sys.exit(1)
