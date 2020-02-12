41abe824996a6d74e5192f256cc1fa6c3a32f4e8
Ben <andreabenini@users.noreply.github.com>
2020-02-12 09:00:59 +0100

    formatting and line number

--- src/history.py
+++ src/history.py
@@ -3,4 +3,4 @@ import readline
 # Print shell command line history
 def History():
     for i in range(readline.get_current_history_length()):
-        print(readline.get_history_item(i + 1))
+        print("%3d %s" % (i+1, readline.get_history_item(i + 1)))
