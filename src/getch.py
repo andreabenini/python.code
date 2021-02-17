8df9570bda57238049fa9e5a8d66d54e2c6a3282
Ben <andreabenini@users.noreply.github.com>
2021-02-17 09:54:07 +0100

    Get a single key from keyboard
    NO CR required, based on termios settings so unix only

---
+++ src/getch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#
+import sys, termios, tty, os, time
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+if getch() == '1':
+    print("1 - Pressed")
+
+#
