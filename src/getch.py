d4750d7dbbf900d0c9109f3429c8dde101d6fb01
Ben <andreabenini@users.noreply.github.com>
2021-02-17 10:47:41 +0100

    getch, curses way

--- src/getch.py
+++ src/getch.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #
-#
+# termios solution
 import sys, termios, tty, os, time
- 
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -12,8 +11,24 @@ def getch():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
-
 if getch() == '1':
     print("1 - Pressed")
 
-#
+# curses alternative
+import curses, time
+def input_char(message):
+    try:
+        win = curses.initscr()
+        win.addstr(0, 0, message)
+        while True: 
+            ch = win.getch()
+            if ch in range(32, 127): break
+            time.sleep(0.05)
+    except: raise
+    finally:
+        curses.endwin()
+    return chr(ch)
+#--------------------------------------
+c = input_char('Press s or n to continue:')
+if c.upper() == 'S':
+    print 'YES'
