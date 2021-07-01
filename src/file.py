cae92ceeaa8e1ea438a77ae73ae1f6458712f241
Ben <andreabenini@users.noreply.github.com>
2021-07-01 14:56:43 +0200

    Update file.py

--- src/file.py
+++ src/file.py
@@ -1,5 +1,6 @@
 # Various methods for opening files:
-
+with open("filename.txt", "rt") as f:
+    data_lines = f.readlines()
 
 # Open pipe for writing, non blocking
 def openAndWrite_NonBlocking():
@@ -15,3 +16,25 @@ def openAndWrite_NonBlocking():
             print("ERROR: Cannot write myfile [OS ERROR]: {}".format(str(e)))
     except Exception as e:
         print("ERROR: {}".format(str(e)))
+
+        
+# Object, serialization, json and so on...
+
+# Save python object to file
+import pickle as pkl
+# Save object
+with open("file.pickle", "wb") as f:
+    pkl.dump(object, f) 
+# Load object
+with open("file.pickle", "rb") as f:
+    object = pkl.load(f)
+
+# Load CSV as a python list of dicts
+import csv
+with open("data.csv") as csv_file:
+    list_of_dicts = list(csv.DictReader(csv_file))
+
+# JSON string to python dict, and back
+import json
+json_dict = json.loads(json_formatted_string)
+json_formatted_string = json.dumps(json_dict)
