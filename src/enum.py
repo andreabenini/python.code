762a716546a81881001e084a179a69adaea50bda
Ben <andreabenini@users.noreply.github.com>
2021-12-22 15:03:33 +0100

    Getting name from enum position

--- src/enum.py
+++ src/enum.py
@@ -1,9 +1,13 @@
 # Simplest method ever: one function, one var
 def enum(**enums):
     return type('Enum', (), enums)
+def enumNames(enumObj):
+    fields = [field for field in enumObj.__dict__.keys() if not field.startswith('_')]
+    return fields
 
 DEBUG = enum(OFF=0, ON=1, VERBOSE=2)
 print(DEBUG.OFF, DEBUG.ON, DEBUG.VERBOSE)
+print(enumNames(DEBUG)[1])  # 'ON'
 
 # With a class, overhead and heavy but classy:
 from enum import Enum
